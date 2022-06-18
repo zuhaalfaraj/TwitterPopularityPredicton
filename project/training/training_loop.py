d431daccb6ead3bbed5a111b92ba7e9944636907
zuha <zuha.alfaraj@gmail.com>
2022-06-18 13:59:58 +0300

    isolater wandb logs from the training loop

--- project/training/training_loop.py
+++ project/training/training_loop.py
@@ -5,12 +5,11 @@ from ..data.s3_connection import S3Connection
 
 
 class TrainingLoop:
-    def __init__(self, model, criterion, optimizer, device, log_to_wandb=True):
+    def __init__(self, model, criterion, optimizer, device):
         self.model = model
         self.criterion = criterion
         self.optimizer = optimizer
         self.device = device
-        self.log_to_wandb = log_to_wandb
 
     def one_batch(self, batch, train=True):
         batch = {k: v.to(self.device) for k, v in batch.items()}
@@ -33,60 +32,42 @@ class TrainingLoop:
 
         return loss
 
+    def batch_loop(self, batch):
+        running_loss, avg_loss = 0, 0
+        for i, b in enumerate(batch):
+            loss = self.one_batch(b, train=True)
+            running_loss += loss
+            avg_loss = running_loss / (i + 1)
+
+        return avg_loss
+
     def training_loop(self, n_epoch, save_model_path, train_loader, valid_loader):
         epoch_number = 0
         best_vloss = 1_000_000.
 
         for epoch in range(n_epoch):
             print('EPOCH {}:'.format(epoch_number + 1))
-            avgtrain_loss = 0
-            avgval_loss = 0
-
-            running_training_loss = 0
-            running_val_loss = 0
 
             self.model.train()
             with tqdm(train_loader, unit="training loop") as batch:
-                for i, b in enumerate(batch):
-                    loss = self.one_batch(b, train=True)
-
-                    running_training_loss += loss
-                    avgtrain_loss = running_training_loss / (i + 1)
-
-                    if not i % 50 and self.log_to_wandb:
-                        wandb.log({'running_train_loss': avgtrain_loss})
-
-            if self.log_to_wandb:
-                logged_metrics = {'train_loss_per_epoch': avgval_loss}
-                wandb.log(logged_metrics)
+                avgtrain_loss = self.batch_loop(batch)
 
             self.model.eval()
             with tqdm(valid_loader, unit="validation loop") as batch:
-                for i, b in enumerate(batch):
-                    loss = self.one_batch(b, train=False)
-
-                    running_val_loss += loss
-
-                    avgval_loss = running_training_loss / (i + 1)
-
-            if self.log_to_wandb:
-                logged_metrics = {'val_loss_per_epoch': avgval_loss}
-                wandb.log(logged_metrics)
+                avgval_loss = self.batch_loop(batch)
 
             print('LOSS train {} valid {}'.format(avgtrain_loss, avgval_loss))
 
             # Track best performance, and save the model's state
             if avgval_loss < best_vloss:
                 best_vloss = avgval_loss
-                S3Connection.upload_model(save_model_path, self.model.state_dict())
+                S3Connection.upload_model(save_model_path, self.model)
 
             epoch_number += 1
 
         self.model = S3Connection.read_model(save_model_path, self.model)
 
         return self.model
-
-
 
 
 
