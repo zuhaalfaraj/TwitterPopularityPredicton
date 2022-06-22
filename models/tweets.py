2b61ec459dc7005f1628c49c7543211c1d37c1bf
zuha <zuha.alfaraj@gmail.com>
2022-06-22 22:26:04 +0300

    model initialization only once

--- models/tweets.py
+++ models/tweets.py
@@ -12,14 +12,12 @@ class TweetModel:
         self.tokenizer = BertTokenizer.from_pretrained(
             config['model_checkpoint'], do_lower_case=True, do_basic_tokenize=True,
             never_split=None)
-        model = BERTRegressor()
-        self.model = S3Connection().read_model(config['s3_model_dir'], model)
 
-    def get_assessment(self, text, lang):
+    def get_assessment(self, text, lang, model):
         text = self.preprocessor.clean(text,lang)
         input_record = self.tokenizer(text, **config['tokenizer_param'])
         input_record = {key: torch.tensor(val).reshape(1,-1) for key, val in input_record.items()}
-        output = self.model(**input_record)
+        output = model(**input_record)
         return output
 
 
--- resources/tweet.py
+++ resources/tweet.py
@@ -1,5 +1,12 @@
 from flask_restful import Resource, reqparse
 from models.tweets import TweetModel
+from project.training.model import BERTRegressor
+from project.data.s3_connection import S3Connection
+from config import config
+
+
+model = S3Connection().read_model(config['s3_model_dir'], BERTRegressor())
+tweet_model = TweetModel()
 
 
 class TweetResource(Resource):
@@ -13,12 +20,9 @@ class TweetResource(Resource):
                        required=True,
                        help="This field cannot left blank")
 
-    def __init__(self):
-        self.tweet_model = TweetModel()
-
     def get(self):
         data = TweetResource.parse.parse_args()
-        out = self.tweet_model.get_assessment(*data)
+        out = tweet_model.get_assessment(*data, model= model)
         if out:
             return {'Assessment result': float(out)}
         return {"message": "not found"}, 404
