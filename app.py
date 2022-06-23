8d8a56e491088f03afaab50a83d08243af5ca547
zuha <zuha.alfaraj@gmail.com>
2022-06-23 22:51:01 +0300

    added travis yaml file and docker compose

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from resources.tweet import TweetResource
 from project.training.model import BERTRegressor
+from flask import request
 
 
 app = Flask(__name__)
@@ -10,6 +11,19 @@ api = Api(app)
 
 
 api.add_resource(TweetResource, '/assessment')
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.get('/shutdown')
+def shutdown():
+    shutdown_server()
+    return {'message':'Server shutting down...'}
 
 
 if __name__ == '__main__':
